81262ab5cef111baf2c0ff03b71aff5f2d2a1e29
Mohak Jain <jmohak30@gmail.com>
2025-03-15 20:19:51 +0530

    Remove

--- couponfrontend/src/main.jsx
+++ couponfrontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='https://github.com/Mohak0325/CouponDistributor'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
